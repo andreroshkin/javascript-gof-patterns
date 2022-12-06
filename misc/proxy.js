b84e38c042f02ee19ca3526ecd57787ef1ae8c8a
Andrey Eroshkin <andreroshkin@gmail.com>
2022-12-06 23:56:53 +0300

    proxy sample 2

--- misc/proxy.js
+++ misc/proxy.js
@@ -15,3 +15,17 @@ dictionary = new Proxy(dictionary, {
 
 console.log(dictionary["Hello"]); // Hola
 console.log(dictionary["Welcome to Proxy"]); // Welcome to Proxy
+
+let range = {
+    start: 1,
+    end: 10,
+};
+
+range = new Proxy(range, {
+    has(target, prop) {
+        return prop >= target.start && prop <= target.end;
+    },
+});
+
+console.log(5 in range); 
+console.log(50 in range); 
